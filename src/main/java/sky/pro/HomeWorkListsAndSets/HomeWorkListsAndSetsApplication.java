3ec18140a3b877cfee03e5aa78e46f34f071e36b
Ivan-Kovalev <gangrenresdwa@gmail.com>
2024-04-01 22:33:34 +0300

    master

---
+++ src/main/java/sky/pro/HomeWorkListsAndSets/HomeWorkListsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.HomeWorkListsAndSets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkListsAndSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkListsAndSetsApplication.class, args);
+	}
+
+}
